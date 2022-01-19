7b2e41f2ff156d7c73131170703804b253ba5794
Jamie <jamienolan93@hotmail.co.uk>
2022-01-19 14:54:11 +0000

    added form

--- client/src/components/BookingForm.js
+++ client/src/components/BookingForm.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { postBooking } from '../services/BookingService';
 
- const BookingForm = () => {
+const BookingForm = ({ addBooking }) => {
 
-  
+  const [formData, setFormData] = useState({})
+
+  const onChange = (e) => {
+    formData[e.target.id] = e.target.value
+    setFormData = formData
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    postBooking(formData).then((data) => addBooking(data));
+    setFormData()
+  }
 
 
-  return( 
-          <form onSubmit={onSubmit} id="bookings-form" >
-            <h2>Add a Booking</h2>
-            <div>
-                <label htmlFor="name">Guest:</label>
-                <input onChange={onChange} type="text" id="name"  />
-            </div>
-            <div className="formWrap">
-                <label htmlFor="email">Email address:</label>
-                <input onChange={onChange} type="email" id="email"  />
-            </div>
+  return (
+    <form onSubmit={onSubmit} id="bookings-form" >
+      <h2>Add a Booking</h2>
+      <div>
+        <label htmlFor="name">Guest:</label>
+        <input onChange={onChange} type="text" id="name" />
+      </div>
+      <div className="formWrap">
+        <label htmlFor="email">Email address:</label>
+        <input onChange={onChange} type="email" id="email" />
+      </div>
 
-            <input type="submit" value="Save" id="save"/>
-	    </form>
+      <input type="submit" value="Save" id="save" />
+    </form>
   )
 };
 
--- client/src/containers/BookingContainer.js
+++ client/src/containers/BookingContainer.js
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import BookingForm from '../components/BookingForm';
 import BookingList from '../components/BookingList';
-import {getBookings} from '../services/BookingService'
+import { getBookings } from '../services/BookingService'
 
 
 const BookingContainer = () => {
 
   const [bookings, setBooking] = useState([])
+
   useEffect(() => {
     getBookings()
-    .then(bookings => setBooking(bookings))
+      .then(bookings => setBooking(bookings))
   }, [])
 
+  const addBooking = (booking) => {
+    const temp = bookings.map(booking => booking);
+    temp.push(booking);
+    setBooking(temp);
+  }
+
   return (
-  <div>
-    <BookingForm/>
-    <BookingList bookings={bookings}/>
-  </div>
+    <div>
+      <BookingForm addBooking={addBooking} />
+      <BookingList bookings={bookings} />
+    </div>
   );
 };
 
