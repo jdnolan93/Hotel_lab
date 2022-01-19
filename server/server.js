b77aa0d330fcffc63c2bdc49f14c425ab6383a0e
peterwilliams <peterkennethwilliams@gmail.com>
2022-01-19 11:52:07 +0000

    server setup first route

--- server/server.js
+++ server/server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
-// const createRouter = require('./helpers/create_router');
+const createRouter = require('./helpers/create_router');
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db('hotel_bookings');
+    const bookingsCollection = db.collection('bookings');
+    const bookingsRouter = createRouter(bookingsCollection);
+    app.use('/api/bookings', bookingsRouter);
+  })
+  .catch(console.err);
 
 
 
